87a2b53d017de8ef36c838862b8693b53dd72e58
Alexanderjsv <alexandersv2003@gmail.com>
2023-06-01 10:27:57 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define col 3
+#define fil 3
+
+int main(){
+    int matriz[fil][col];
+
+
+    for(int i;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("Ingrese el numero de la posicion %d, %d : ",i,j);
+            scanf("%d",&matriz[i][j]);
+        }
+    }
+
+    for(int i=0;i<fil;i++){
+        for (int j=0;j<col;j++){
+            printf("%d ",matriz[i][j]);
+            if(j==col-1){
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
